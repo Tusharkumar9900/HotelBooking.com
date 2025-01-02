f287660abf12afb0c1ba5dacffdecd8496b4b2df
Tusharkumar9900 <tusharkumarr0809@gmail.com>
2025-01-02 23:16:37 +0530

    commit

--- hotelbookingfrontened/hotelbooking/src/components/room/AddRoom.jsx
+++ hotelbookingfrontened/hotelbooking/src/components/room/AddRoom.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 const AddRoom = () => {
+  const[newRoom, setNewRoom] = useState({
+    photo : null,
+    roomType : "",
+    roomPrice : ""
+  })
+  const[imagePreview, setImagePreview] = useState("")
+  const[successMessage, setSuccessMessage] = useState("")
+  const[errorMessage, setErrorMessage] = useState("")
+
+  const handleRoomInputChange = (e) => {
+    const name = e.target.name
+    let value = e.target.value
+    if(name === "roomPrice"){
+      if(!isNaN(value)){
+        value.parseInt(value)
+      }else{
+        value = ""
+      }
+      
+    }
+
+    setNewRoom({...newRoom, [name]: value})
+  }
+    
+  
+
   return (
     <div>AddRoom</div>
   )
 }
 
 export default AddRoom
+
